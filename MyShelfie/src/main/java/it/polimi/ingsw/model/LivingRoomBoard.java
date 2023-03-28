8182327d8f7284c8e78161acdc8e7a3ace8b46e6
Carlo <carlo.terzuoli@mail.polimi.it>
2023-03-28 10:45:20 +0200

    - help, files are changing as harry potter's stairs

--- MyShelfie/src/main/java/it/polimi/ingsw/model/LivingRoomBoard.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/LivingRoomBoard.java
@@ -3,33 +3,51 @@ package it.polimi.ingsw.model;
 
 import java.util.List;
 
+/**
+ * Game's board
+ * @author Carlo
+ */
 class LivingRoomBoard(){
+    // The board is represented as a MAX_WIDTH x MAX_HEIGHT matrix of Spaces
     private final int MAX_WIDTH = 9;
     private final int MAX_HEIGHT = 9;
     private Space[][] spaces = new Space[][];
     private int numberOfPlayers;
 
-    private final int[][] twoPlayerSetup = {{9,0,0},{3,2,4},{3,3,4},{2,6,1},   {7,1,1},   {1,6,2},{3,3,3},{4,2,3},{9,0,0,}}; // PROVA
-    private final int[][] threePlayerSetup = {{9,0,0},{3,2,4},{3,3,4},{2,6,1},   {7,1,1},   {1,6,2},{3,3,3},{4,2,3},{9,0,0,}}; // PROVA
-    private final int[][] fourPlayerSetup = {{9,0,0},{3,2,4},{3,3,4},{2,6,1},   {7,1,1},   {1,6,2},{3,3,3},{4,2,3},{9,0,0,}}; // PROVA
+    // The followings are the setups for each possible number of players.
+    // Each sublist has [0] and [2] indexes for FORBIDDEN and [1] for ACTIVE
+    // This could change if we decide to use the THREE_PLAYER and FOUR_PLAYER SpaceType's values
+    private final int[][] twoPlayerSetup = {{9,0,0},{3,2,4},{3,3,3},{2,6,1},   {1,7,1},   {1,6,2},{3,3,3},{4,2,3},{9,0,0,}};
+    private final int[][] threePlayerSetup = {{3,1,5},{3,2,4},{2,5,2},{2,7,0},   {1,7,1},   {0,7,2},{2,4,3},{4,2,3},{9,0,0,}};
+    private final int[][] fourPlayerSetup = {{3,2,4},{3,3,3},{3,3,4},{2,6,1},   {1,7,1},   {1,6,2},{3,3,3},{4,2,3},{9,0,0,}};
 
 
     /**
-     * Initialization of game board
-     * @author Carlo, FraMabe
+     * Initialization of game's board
+     * Takes as input the number of players, and it initializes the board accordingly
      * @param numberOfPlayers int
+     * @author Carlo
      */
-    public LivingRoomBoard(int numberOfPlayers){
+    public LivingRoomBoard(int numberOfPlayers) throws IllegalArgumentException {
         int[][] activeList = null;
 
         switch (numberOfPlayers){
-            case 2: activeList = twoPlayerSetup;
-            case 3: activeList = threePlayerSetup;
-            case 4: activeList = fourPlayerSetup;
-            default: activeList = twoPlayerSetup;
+            case 2: {
+                activeList = twoPlayerSetup;
+                break;
+            }
+            case 3: {
+                activeList = threePlayerSetup;
+            }
+            case 4: {
+                activeList = fourPlayerSetup;
+            }
+            default: {
+                throw new IllegalArgumentException();
+            }
         }
 
-        int c,r = 0;
+        int c = 0, r = 0;
         for(int[] list: activeList){
             // forbidden tiles
             for(int i=0; i<list[0]; i++){
@@ -55,7 +73,9 @@ class LivingRoomBoard(){
 
     }
 
-    public ItemTile getTile(){
+
+
+    public ItemTile getTile(Position position){
         // return ItemTile
         return null;
     }
@@ -71,6 +91,7 @@ class LivingRoomBoard(){
         return null;
     }
 
+    // This method does not make much sense
     public boolean allSpaceHaveAdjacent(){
         //return boolean
         return false;
