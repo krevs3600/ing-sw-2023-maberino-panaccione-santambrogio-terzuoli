e8a58aca1be762928103e516a6b77a89dda5ed04
fra-mabe29 <francesco.mabe@gmail.com>
2023-03-29 17:28:34 +0200

    Javadoc for classes Bag and Space

--- MyShelfie/src/main/java/it/polimi/ingsw/model/Space.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/Space.java
@@ -1,29 +1,57 @@
 package it.polimi.ingsw.model;
 
+/**
+ * Class Space <br>
+ * it consists of all the single spaces that make up the LivingRoomBoard
+ */
+
+
 public class Space {
     private boolean free;
     private SpaceType type;
     private Position position;
     private ItemTile tile;
 
+    /**
+     * Class contructor
+     * @param type type of the space
+     * @param position position of the space on the board
+     */
     public Space(SpaceType type, Position position){
         this.type = type;
         this.position = position;
         this.tile = null;
     }
 
+    /**
+     *
+     * @return the type of the space
+     */
     public SpaceType getType(){
         return type;
     }
 
+    /**
+     *
+     * @return whether the space is free or occupied
+     */
     public boolean isFree(){
         return free;
     }
 
+    /**
+     *
+     * @return the position the space is in
+     */
     public Position getPosition(){
         return position;
     }
 
+    /**
+     * Places an ItemTile on the space
+     * @param tile the tile to be placed on the space
+     * @throws IllegalAccessError if the space was not free
+     */
     public void setTile(ItemTile tile) throws IllegalAccessError{
         if (getType() == SpaceType.DEFAULT) {
             this.tile = tile;
@@ -32,13 +60,23 @@ public class Space {
             throw new IllegalAccessError();
         }
     }
+
+    /**
+     *
+     * @return the tile that is placed on the space
+     */
     public ItemTile getTile(){
         return this.tile;
 
     }
 
+    /**
+     * takes the ItemTile from the space.
+     * @return The ItemTile that was taken
+     * @throws IllegalAccessError if the space was free or not playable
+     */
     public ItemTile drawTile() throws IllegalAccessError{
-        if (this.free){
+        if (!isFree()){
             ItemTile tempTile = this.tile;
             this.tile = null;
             this.free = false;
