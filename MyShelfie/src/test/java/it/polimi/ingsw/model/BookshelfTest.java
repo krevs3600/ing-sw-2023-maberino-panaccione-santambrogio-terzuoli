9c108d3b9383f12da06b0e3228e7d118667287c5
Francesca Pia Panaccione <francescapia.panaccione@mail.polimi.it>
2023-04-27 10:29:29 +0200

    BookshelfTest.java creation

--- MyShelfie/src/test/java/it/polimi/ingsw/model/BookshelfTest.java
+++ MyShelfie/src/test/java/it/polimi/ingsw/model/BookshelfTest.java
@@ -1,73 +1,14 @@
 package it.polimi.ingsw.model;
 
-import junit.framework.TestCase;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class BookshelfTest {
- @Test
-    public void correctNumberofInsertableTilesForOneColumn(){
-     Bookshelf b=new Bookshelf();
-     Map<Integer, Integer> m = new HashMap<>();
-     TilePack tp=new TilePack();
-     TilePack tp1=new TilePack();
-     TilePack tp2=new TilePack();
-     ItemTile i1=new ItemTile(TileType.CAT);
-     ItemTile i2=new ItemTile(TileType.BOOK);
-     ItemTile i3=new ItemTile(TileType.BOOK);
-     tp.insertTile(i1);
-     tp.insertTile(i2);
-     tp.insertTile(i3);
-     tp1.insertTile(i1);
-     tp1.insertTile(i2);
-     tp1.insertTile(i3);
-     tp2.insertTile(i1);
-     tp2.insertTile(i2);
-     tp2.insertTile(i3);
-     b.insertTile(tp,1);
-     b.insertTile(tp1,2);
-     b.insertTile(tp2,3);
-     assertEquals(6 ,b.getNumberInsertableTilesColumn(1));
-     assertEquals(m,b.getNumberAdjacentTiles(TileType.BOOK));
- }
-    @Test
-    public void AllEmptyColumn(){
-     Bookshelf b=new Bookshelf();
-        assertEquals(6,b.getNumberInsertableTiles());
-
-    }
-
- @Test
- public void AdjacentTest(){
-  Bookshelf b=new Bookshelf();
-  Map<Integer, Integer> m = new HashMap<>();
-  TilePack tp=new TilePack();
-  TilePack tp1=new TilePack();
-  TilePack tp2=new TilePack();
-  ItemTile i1=new ItemTile(TileType.CAT);
-  ItemTile i2=new ItemTile(TileType.BOOK);
-  ItemTile i3=new ItemTile(TileType.BOOK);
-  tp.insertTile(i1);
-  tp.insertTile(i2);
-  tp.insertTile(i3);
-  tp1.insertTile(i1);
-  tp1.insertTile(i2);
-  tp1.insertTile(i3);
-  tp2.insertTile(i1);
-  tp2.insertTile(i2);
-  tp2.insertTile(i3);
-  b.insertTile(tp,1);
-  b.insertTile(tp1,2);
-  b.insertTile(tp2,3);
-  m.put(6,1);
-  assertEquals(m,b.getNumberAdjacentTiles(TileType.BOOK));
-
-
-
- }
+class BookshelfTest {
+}
 }
