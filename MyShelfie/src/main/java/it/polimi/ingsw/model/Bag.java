e496cf566d7a134621a43904dab42a1daab3b839
Francesco Santambrogio <francesco2.santambrogio@mail.polimi.it>
2023-04-28 17:37:41 +0200

    Bag and Bookshelf classes updated, Bag Javadoc about exceptions, Bookshelf Javadoc

--- MyShelfie/src/main/java/it/polimi/ingsw/model/Bag.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/Bag.java
@@ -6,13 +6,16 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * Class Bag: it contains all the item tiles that are not placed on the board or on the Players' shelves
+ * <h1>Class Bag</h1>
+ * The class Bag contains all the item tiles that are not placed on the board or on the Players' shelves
+ *
+ * @author Carlo Terzuoli, Francesco Maberino
+ * @version 1.0
+ * @since 3/28/2023
  */
 
 
-/**
- * Class contructor
- */
+
 public class Bag {
     private static final int MAX_SIZE = 132;
     private static final int MAX_SIZE_PER_TYPE = 22;
@@ -21,6 +24,9 @@ public class Bag {
     private int size = 0;
     private List<ItemTile> bag = new ArrayList<ItemTile>();;
 
+    /**
+     * Class contructor
+     */
     public Bag(){
         for(TileType type: TileType.values()){
             for(int i=0; i<MAX_SIZE_PER_TYPE; i++){
@@ -31,8 +37,9 @@ public class Bag {
     }
 
     /**
-     * Used in order to draw from the bag a random item tile, that will be later placed on the board
-      * @return the item that was drawn from the bag
+     * This method is used to draw from the bag a random item tile that will be later placed on the board
+     * @return ItemTile It returns the item that was drawn from the bag
+     * @exception IndexOutOfBoundsException The exception is thrown if the method is called when there are no tiles inside the bag
      */
     public ItemTile drawTile() throws IndexOutOfBoundsException{
         if(this.size > 0){
@@ -50,10 +57,11 @@ public class Bag {
     }
 
     /**
-     * Used in order to take from the bag a specific amount of random item tiles, that will later be
+     * This method is used to take from the bag a specific amount of random item tiles, that will later be
      * placed on the board
      * @param amount number of tiles to be drawn from the bag
-     * @return the item tiles that were picked from the bag
+     * @return List<ItemTile> It returns the item tiles that were picked from the bag
+     * @exception IllegalArgumentException The exception is thrown if the amount of items to be drawn exceeds the size of the bag
      */
     public List<ItemTile> drawTile(int amount) throws IllegalArgumentException{
         if (amount >= 0 && amount <= this.getSize()) {
@@ -73,8 +81,9 @@ public class Bag {
     }
 
     /**
-     * Inserts in the bag a list of item tiles
+     * This method inserts in the bag a list of item tiles
      * @param leftovers list of item tiles to be inserted in the bag
+     * @exception IllegalArgumentException The exception is thrown if the number of item tiles to insert exceeds the available space of the bag
      */
     public void insertTiles(List<ItemTile> leftovers) throws IllegalArgumentException{
         if (this.getSize() + leftovers.size() < MAX_SIZE) {
@@ -89,7 +98,8 @@ public class Bag {
     }
 
     /**
-     * @return how many item tiles are left in the bag
+     * This getter method gets the size of the bag
+     * @return int It returns how many item tiles are left in the bag
      */
     public int getSize(){
         return size;
