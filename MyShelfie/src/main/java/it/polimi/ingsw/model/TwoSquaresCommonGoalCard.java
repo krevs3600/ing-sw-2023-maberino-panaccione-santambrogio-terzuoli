39e13f722c6610b9fd0741144d55419b0250c1ff
Francesca Pia Panaccione <francescapia.panaccione@mail.polimi.it>
2023-04-29 16:59:49 +0200

    TwoSquaresCommonGoalCard fixed(null value and  adding the equal type check between the two squares) and tested

--- MyShelfie/src/main/java/it/polimi/ingsw/model/TwoSquaresCommonGoalCard.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/TwoSquaresCommonGoalCard.java
@@ -10,38 +10,48 @@ public class TwoSquaresCommonGoalCard extends CommonGoalCard{
     }
 
     public boolean CheckPattern (Bookshelf b) {
-
-     int squares=0;
+        TileType tipeFound = TileType.CAT; // random initialitation
+        int squares = 0;
         int[][] auxiliary = new int[b.getMaxHeight()][b.getMaxWidth()];
-        for (int i = 0; i < b.getMaxWidth(); i++) {
-            for (int j = 0; j < b.getMaxHeight(); j++) {
+        for (int i = 0; i < b.getMaxHeight(); i++) {
+            for (int j = 0; j < b.getMaxWidth(); j++) {
                 auxiliary[i][j] = 0;
             }
         }
 
         if (toBeChecked(b)) {
-            for(int i=0; i< b.getMaxWidth() && squares<2 ;i++){
-                for(int j=0;j<b.getMaxHeight() && squares<2 ;j++){
-                    if(b.getGrid()[i][j].getType().equals(b.getGrid()[i+1][j].getType()) && auxiliary[i][j]==0 &&
-                            b.getGrid()[i][j].getType().equals(b.getGrid()[i][j+1].getType()) && auxiliary[i][j+1]==0 &&
-                             b.getGrid()[i][j].getType().equals(b.getGrid()[i+1][j+1].getType()) && auxiliary[i+1][j+1]==0){
-                        auxiliary[i][j]=1;
-                        auxiliary[i+1][j]=1;
-                        auxiliary[i][j+1]=1;
-                        auxiliary[i+1][j+1]=1;
-                        squares++;
+            for (int i = 0; i < b.getMaxHeight() - 1 && squares < 2; i++) {
+                for (int j = 0; j < b.getMaxWidth() - 1 && squares < 2; j++) {
+                    if (b.getGrid()[i][j] != null && b.getGrid()[i + 1][j] != null
+                            && b.getGrid()[i][j + 1] != null && b.getGrid()[i + 1][j + 1] != null) {
+                        if (squares == 0 && b.getGrid()[i][j].getType().equals(b.getGrid()[i + 1][j].getType()) && auxiliary[i][j] == 0 &&
+                                b.getGrid()[i][j].getType().equals(b.getGrid()[i][j + 1].getType()) && auxiliary[i][j + 1] == 0 &&
+                                b.getGrid()[i][j].getType().equals(b.getGrid()[i + 1][j + 1].getType()) && auxiliary[i + 1][j] == 0) {
+                            auxiliary[i][j] = 1;
+                            auxiliary[i + 1][j] = 1;
+                            auxiliary[i][j + 1] = 1;
+                            auxiliary[i + 1][j + 1] = 1;
+                            tipeFound = b.getGrid()[i][j].getType();
+                            squares++;
+                        } else if (squares > 0 && b.getGrid()[i][j].getType().equals(tipeFound) && b.getGrid()[i][j].getType().equals(b.getGrid()[i + 1][j].getType()) && auxiliary[i][j] == 0 &&
+                                b.getGrid()[i][j].getType().equals(b.getGrid()[i][j + 1].getType()) && auxiliary[i][j + 1] == 0 &&
+                                b.getGrid()[i][j].getType().equals(b.getGrid()[i + 1][j + 1].getType()) && auxiliary[i + 1][j] == 0) {
+                            auxiliary[i][j] = 1;
+                            auxiliary[i + 1][j] = 1;
+                            auxiliary[i][j + 1] = 1;
+                            auxiliary[i + 1][j + 1] = 1;
+                            squares++;
 
-                }
+                        }
+
+                    }
                 }
             }
-           return (squares==2);
+        }
 
-            }
+            return (squares== 2);
 
-
-
-            else {
-            return false;
         }
     }
-}
+
+
