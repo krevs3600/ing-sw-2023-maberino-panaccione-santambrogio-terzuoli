0d05eccf74149d48a6ab559b3eacb56901ecae84
Francesca Pia Panaccione <francescapia.panaccione@mail.polimi.it>
2023-05-08 17:50:36 +0200

    fixes on JavaFxMain.java and module-info.java

--- MyShelfie/src/main/java/it/polimi/ingsw/client/view/FXML/JavaFxMain.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/view/FXML/JavaFxMain.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class JavaFxMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("start.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("start_scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- MyShelfie/src/main/java/module-info.java
+++ MyShelfie/src/main/java/module-info.java
@@ -1,7 +1,12 @@
-module PSP47 {
+module PSP47{
     requires javafx.fxml;
     requires javafx.controls;
     requires json.simple;
 
+
+
     opens it.polimi.ingsw.client.view.FXML;
+
+
+
 }
