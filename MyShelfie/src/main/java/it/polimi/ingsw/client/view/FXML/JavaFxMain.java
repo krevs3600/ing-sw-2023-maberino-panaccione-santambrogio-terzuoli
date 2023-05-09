f71ca328934c5bbf2eedbc59944d2387dc7ec351
Francesca Pia Panaccione <francescapia.panaccione@mail.polimi.it>
2023-05-09 18:25:07 +0200

    JavaFx update

--- MyShelfie/src/main/java/it/polimi/ingsw/client/view/FXML/JavaFxMain.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/view/FXML/JavaFxMain.java
@@ -6,20 +6,35 @@ import javafx.stage.Stage;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-
+import javafx.scene.Parent;
 import java.io.IOException;
+import java.io.File;
+import java.net.URL;
+import javafx.scene.paint.Color;
+
+
 
 public class JavaFxMain extends Application {
+    private static Stage window;
+
     @Override
     public void start(Stage stage) throws IOException {
+
+
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("start_scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        window=stage;
     }
     public static void main(String[] args) {
         launch();
+    }
+    public static Stage getWindow(){
+        return window;
     }
 
 }
