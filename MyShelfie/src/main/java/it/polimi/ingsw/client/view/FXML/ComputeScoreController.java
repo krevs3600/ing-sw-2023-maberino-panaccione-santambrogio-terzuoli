25830f1ecb1ce7dab3e5ea6b97e3900d975083c4
Carlo <carlo.terzuoli@mail.polimi.it>
2023-05-30 18:24:27 +0200

    ComputeScoreController base

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/client/view/FXML/ComputeScoreController.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.client.view.FXML;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class ComputeScoreController {
+    @FXML
+    public GridPane bookshelf;
+    @FXML
+    public ImageView commonGoalCard1;
+    @FXML
+    public ImageView commonGoalCard2;
+    @FXML
+    public ImageView endGameToken;
+    @FXML
+    public ImageView personalGoalCard;
+    @FXML
+    public Label commonPoint2;
+    @FXML
+    public Label commonPoint1;
+    @FXML
+    public Label personalGoalPoint;
+    @FXML
+    public Label endGamePoint;
+    @FXML
+    public Label generalPoint;
+    @FXML
+    public Label totalScore;
+    @FXML
+    public GridPane otherBookshelf;
+}
